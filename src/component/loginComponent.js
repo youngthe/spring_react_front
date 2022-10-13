98744f57a83243c785097f7cba240d6d1ca9c33e
Young Jun <whdudwns7100@naver.com>
2022-10-13 23:19:12 +0900

    feat : token validate

--- src/component/loginComponent.js
+++ src/component/loginComponent.js
@@ -21,6 +21,25 @@ function App() {
         }
     }
 
+    const search = () => {
+        console.log(localStorage.getItem("jwt_token"))
+        axios.post('/search', {
+            jwt_token : localStorage.getItem("jwt_token")
+        })
+            .then(function(res){
+                if(res.data.resultCode == "jwt-error"){
+                    alert("토큰이 만료되었습니다.");
+                    logout();
+                }else{
+                    alert("success search");
+                }
+            }).catch(function(error){
+            console.log(error);
+        })
+
+    }
+
+
     const onSubmit= (event) => {
         event.preventDefault(); // submit 시 웹페이지가 리로딩 되는걸 막아줌
         console.log(userID, Password);
@@ -34,9 +53,12 @@ function App() {
             .then(function(res){
                 if(res.data.resultCode == "true"){
                     console.log("success");
+                    alert("로그인 성공");
+                    localStorage.setItem("jwt_token", res.data.jwt_token);
                     login();
                 }else{
                     logout();
+                    alert("로그인 실패");
                     console.log("false");
                 }
             }).catch(function(error){
@@ -59,7 +81,11 @@ if(loginState == 0){
     return (
         <div>
             <h1>로그인 성공</h1>
+
+            <input type="button" onClick={search} value="검색"></input>
             <input type="button" onClick={logout} value="로그아웃"></input>
+
+
         </div>
     );
 }
